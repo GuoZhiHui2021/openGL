35f6a0d5793e6564ffcd286600140805103ce6a3
郭志辉 <guozhihui@sandboxol.com>
2022-01-06 20:46:52 +0800

    练习

--- MyFristOpenGLWindow/src/main.cpp
+++ MyFristOpenGLWindow/src/main.cpp
@@ -33,28 +33,35 @@ int main()
 		
 		ShaderManager::Instance()->initialize();
 
-		//render.setPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		glm::vec3 cubePositions[] = {
+			  glm::vec3(0.0f,  0.0f,  0.0f),
+			  glm::vec3(2.0f,  5.0f, -15.0f),
+			  glm::vec3(-1.5f, -2.2f, -2.5f),
+			  glm::vec3(-3.8f, -2.0f, -12.3f),
+			  glm::vec3(2.4f, -0.4f, -3.5f),
+			  glm::vec3(-1.7f,  3.0f, -7.5f),
+			  glm::vec3(1.3f, -2.0f, -2.5f),
+			  glm::vec3(1.5f,  2.0f, -2.5f),
+			  glm::vec3(1.5f,  0.2f, -1.5f),
+			  glm::vec3(-1.3f,  1.0f, -1.5f)
+		};
 		SystemManager::Instance()->pushSystemCommand(SystemCommand(2, 1, "1"));
-		SystemManager::Instance()->pushSystemCommand(SystemCommand(3, 1, "1"));
-		SystemManager::Instance()->pushSystemCommand(SystemCommand(4, 1, "{1,1}"));
-		SystemManager::Instance()->pushSystemCommand(SystemCommand(4, 1, "{1,2}"));
-		SystemManager::Instance()->pushSystemCommand(SystemCommand(6, 2, "{1,10.0f,10.0f,0.0f}"));
-		SystemManager::Instance()->pushSystemCommand(SystemCommand(6, 3, "{1,0.0f,0.0f,0.0f}"));
-		SystemManager::Instance()->pushSystemCommand(SystemCommand(6, 4, "{1,0.0f,45.0f,0.0f}"));
-		SystemManager::Instance()->pushSystemCommand(SystemCommand(6, 5, "{1,3.0f,1.0f,1.0f}"));
-		SystemManager::Instance()->pushSystemCommand(SystemCommand(7, 1, "{1,true}"));
-		SystemManager::Instance()->pushSystemCommand(SystemCommand(7, 2, "{1,material/default.materialPrototype}"));
+		for (size_t i = 0; i < 10; i++)
+		{
+			
+			SystemManager::Instance()->pushSystemCommand(SystemCommand(3, 1, std::to_string(i+1)));
+			SystemManager::Instance()->pushSystemCommand(SystemCommand(4, 1, "{" + std::to_string(i + 1) + ",1}"));
+			SystemManager::Instance()->pushSystemCommand(SystemCommand(4, 1, "{" + std::to_string(i + 1) + ",2}"));
+			if(!(i%3))
+				SystemManager::Instance()->pushSystemCommand(SystemCommand(6, 2, "{" + std::to_string(i + 1) + ",10.0f,10.0f,0.0f}"));
+			SystemManager::Instance()->pushSystemCommand(SystemCommand(6, 3, "{" + std::to_string(i + 1) + "," + std::to_string(cubePositions[i].x) + "," + std::to_string(cubePositions[i].y) + "," + std::to_string(cubePositions[i].z) + "}"));
+			//SystemManager::Instance()->pushSystemCommand(SystemCommand(6, 4, "{1,0.0f,0.0f,0.0f}"));
+			//SystemManager::Instance()->pushSystemCommand(SystemCommand(6, 5, "{1,1.0f,1.0f,1.0f}"));
+			//SystemManager::Instance()->pushSystemCommand(SystemCommand(7, 1, "{1,true}"));
+			//SystemManager::Instance()->pushSystemCommand(SystemCommand(7, 2, "{1,material/default.materialPrototype}"));
+		}
 
-		SystemManager::Instance()->pushSystemCommand(SystemCommand(2, 1, "2"));
-		SystemManager::Instance()->pushSystemCommand(SystemCommand(3, 1, "2"));
-		SystemManager::Instance()->pushSystemCommand(SystemCommand(4, 1, "{2,1}"));
-		SystemManager::Instance()->pushSystemCommand(SystemCommand(4, 1, "{2,2}"));
-		SystemManager::Instance()->pushSystemCommand(SystemCommand(6, 2, "{2,10.0f,10.0f,0.0f}"));
-		SystemManager::Instance()->pushSystemCommand(SystemCommand(6, 3, "{2,2.0f,0.0f,-5.0f}"));
-		SystemManager::Instance()->pushSystemCommand(SystemCommand(6, 4, "{2,0.0f,45.0f,0.0f}"));
-		SystemManager::Instance()->pushSystemCommand(SystemCommand(6, 5, "{2,1.0f,1.0f,1.0f}"));
-		SystemManager::Instance()->pushSystemCommand(SystemCommand(7, 1, "{2,true}"));
-		SystemManager::Instance()->pushSystemCommand(SystemCommand(7, 2, "{2,material/default.materialPrototype}"));
+		//render.setPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 		while (!glfwWindowShouldClose(window))
 		{
 			// ‰»Î
